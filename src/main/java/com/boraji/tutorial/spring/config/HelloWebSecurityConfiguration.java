d55f56d30e5f6d67d32f683feefb04189b6c7a05
Kirill Karpusha <Kyrylo_Karpusha@epam.com>
2018-03-09 17:39:10 +0200

    update

--- src/main/java/com/boraji/tutorial/spring/config/HelloWebSecurityConfiguration.java
+++ src/main/java/com/boraji/tutorial/spring/config/HelloWebSecurityConfiguration.java
@@ -27,6 +27,7 @@ public class HelloWebSecurityConfiguration
                 .antMatchers("/resources/**"); // #3
     }
 
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
